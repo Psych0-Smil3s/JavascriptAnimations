31cf2d18b89bb3861e0573669e9fbec8bbd94e8f
graemephilipross <graemephilipross@gmail.com>
2014-03-22 12:18:44 -0700

    updated preloader

--- HTML5Game/js/Preloader.js
+++ HTML5Game/js/Preloader.js
@@ -16,7 +16,7 @@ this.Game = this.Game || {};
         var queue = new createjs.LoadQueue(false);
 
         var PreloaderView = new Game.PreloaderView({imageURL:"img/palringo_p_50px.png",
-                                                    imageID:"PalringoLogo"}, queue);
+            imageID:"PalringoLogo"}, queue);
 
         queue.addEventListener("complete", handleComplete);
         queue.addEventListener("progress", PreloaderView.handleProgress);
@@ -84,23 +84,23 @@ this.Game = this.Game || {};
             stage = new createjs.Stage(canvas),
             stageContainer = new createjs.Container();
 
-            canvas.width = utils.getWidth();
-            canvas.height = utils.getHeight();
+        canvas.width = utils.getWidth();
+        canvas.height = utils.getHeight();
 
         var scale = Math.min(canvas.width/BaseWidth,canvas.height/BaseHeight);
 
         var loadingImage = new createjs.Bitmap(o.imageURL);
-            loadingImage.x = canvas.width/2  - (25 * scale) ;
-            loadingImage.y = canvas.height/2 - (65 * scale) ;
-            loadingImage.alpha = 0.0;
-            loadingImage.scaleX = loadingImage.scaleY = scale;
+        loadingImage.x = canvas.width/2  - (25 * scale) ;
+        loadingImage.y = canvas.height/2 - (65 * scale) ;
+        loadingImage.alpha = 0.0;
+        loadingImage.scaleX = loadingImage.scaleY = scale;
 
         var loadProgressLabel = new createjs.Text("Loading...","14px Verdana","black");
-            loadProgressLabel.lineWidth = 200;
-            loadProgressLabel.textAlign = "center";
-            loadProgressLabel.x = canvas.width/2;
-            loadProgressLabel.y = canvas.height/2 + (20 * scale);
-            loadProgressLabel.scaleX = loadProgressLabel.scaleY = scale;
+        loadProgressLabel.lineWidth = 200;
+        loadProgressLabel.textAlign = "center";
+        loadProgressLabel.x = canvas.width/2;
+        loadProgressLabel.y = canvas.height/2 + (20 * scale);
+        loadProgressLabel.scaleX = loadProgressLabel.scaleY = scale;
 
         var loadingBarContainer = new createjs.Container();
         var loadingBarHeight = 10 * scale,
@@ -108,11 +108,11 @@ this.Game = this.Game || {};
             LoadingBarColor = createjs.Graphics.getRGB(0,0,0);
 
         var loadingBar = new createjs.Shape();
-            loadingBar.graphics.beginFill(LoadingBarColor).drawRect(0, 0, 1, loadingBarHeight).endFill();
+        loadingBar.graphics.beginFill(LoadingBarColor).drawRect(0, 0, 1, loadingBarHeight).endFill();
 
         var frame = new createjs.Shape(),
             padding = 5 * scale;
-            frame.graphics.setStrokeStyle(1).beginStroke(LoadingBarColor).drawRect(-padding/2, -padding/2, loadingBarWidth+padding, loadingBarHeight+padding);
+        frame.graphics.setStrokeStyle(1).beginStroke(LoadingBarColor).drawRect(-padding/2, -padding/2, loadingBarWidth+padding, loadingBarHeight+padding);
 
         loadingBarContainer.addChild(loadingBar, frame);
         loadingBarContainer.x = Math.round(canvas.width/2 - loadingBarWidth/2);
@@ -122,8 +122,6 @@ this.Game = this.Game || {};
         stageContainer.addChild(loadProgressLabel);
         stageContainer.addChild(loadingImage);
         stage.addChild(stageContainer);
-		
-		window.addEventListener('resize', resizePreloader, false); // reseize event
 
         this.handleProgress = function() {
 
@@ -154,67 +152,7 @@ this.Game = this.Game || {};
                 createjs.Ticker.removeEventListener("tick", stage);
                 $('.canvas').animate({opacity:1}, 500);
             }
-			
-			// the window event listener remains attached always, we could remove it here, but this seems to not work after a refresh, so leave it attached - it wont affect game 
-			//window.removeEventListener('resize', resizePreloader, false);
         }
-		
-		function resizePreloader() { 
-		    // experiment with these values to get a responsive preloader - so far, image is responsive and so is loading text and bar - but their positions
-			// when moving across screen isn't spot on
-			
-			// remove all children from containers, and reset their x / y
-			stageContainer.removeAllChildren();
-            stageContainer.x = stageContainer.y = 0;
-			
-			loadingBarContainer.removeAllChildren();
-            loadingBarContainer.x = loadingBarContainer.y = 0;
-			
-			// get canvas width / height and scale
-			canvas.width = utils.getWidth();
-            canvas.height = utils.getHeight();
-
-			scale = Math.min(canvas.width/BaseWidth,canvas.height/BaseHeight);
-
-			//for some reason, with the image, always have to re-draw it for it to scale (scaling existing one doesn't work)
-			loadingImage = new createjs.Bitmap(o.imageURL);
-			loadingImage.x = canvas.width/2  - (25 * scale) ;
-			loadingImage.y = canvas.height/2 - (65 * scale) ;
-			//loadingImage.alpha = 0.0; -- this makes it invisible
-			loadingImage.scaleX = loadingImage.scaleY = scale;
-
-			// use existing progress label
-			//loadProgressLabel = new createjs.Text("Loading...","14px Verdana","black");
-			loadProgressLabel.lineWidth = 200;
-			loadProgressLabel.textAlign = "center";
-			loadProgressLabel.x = canvas.width/2;
-			loadProgressLabel.y = canvas.height/2 + (20 * scale);
-			loadProgressLabel.scaleX = loadProgressLabel.scaleY = scale;
-
-			// use existing loading bar
-			//loadingBarContainer = new createjs.Container();
-			loadingBarHeight = 10 * scale;
-			loadingBarWidth = 300 * scale;
-			LoadingBarColor = createjs.Graphics.getRGB(0,0,0);
-
-			//loadingBar = new createjs.Shape();
-			//loadingBar.graphics.beginFill(LoadingBarColor).drawRect(0, 0, 1, loadingBarHeight).endFill();
-
-			frame = new createjs.Shape(),
-			padding = 5 * scale;
-			frame.graphics.setStrokeStyle(1).beginStroke(LoadingBarColor).drawRect(-padding/2, -padding/2, loadingBarWidth+padding, loadingBarHeight+padding);
-
-			loadingBarContainer.addChild(loadingBar, frame);
-			loadingBarContainer.x = Math.round(canvas.width/2 - loadingBarWidth/2);
-			loadingBarContainer.y = Math.round(canvas.height/2 - loadingBarHeight/2);
-			stage.addChild(loadingBarContainer);
-
-			stageContainer.addChild(loadProgressLabel);
-			stageContainer.addChild(loadingImage);
-			stage.addChild(stageContainer);
-			
-			stage.update();
-		}
     }
 
     Game.PreloaderView = PreloaderView;
